eeefac55fedc65ad6d3093d1478d4c2e4990d778
Karn Ashutosh  Singh <karns7844@gmail.com>
2025-10-30 00:33:08 +0530

    navbar bar hero setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,32 @@
 "use client"
 
 import Image from "next/image";
+import Navbar from "@/components/Navbar"
 
+import Container from "@/components/containers"
+import { Inter } from "next/font/google";
 
-import  Container  from "@/components/containers"
-import {Inter} from "next/font/google";
-
-const inter=Inter({weight:["400","500","800","900"]})
+const inter = Inter({ weight: ["400", "500", "800", "900"] })
 
 export default function Home() {
   return (
-        <div className="flex min-h-screen  justify-center  font-sans ">
 
-    <Container className=" min-h-screen">
-      <h1 className="text-2xl md:3xl font-bold tracking-tight  text-neutral-600"> Hi there </h1>
-      
-      <div></div>
-    
-      <p  className="text-neutral-600">&.Like to code and can tell me and build software  yara yara  and also passionate abou open source and
-     <p className="text-neutral-600"> &.yokoso and alos good at one thing com,muncatiing 
 
-     </p>
-    </p>
-    
-    </Container>
+    <div className="flex min-h-screen  justify-center  font-sans ">
+
+
+      <Container className=" min-h-screen">
+        <Navbar></Navbar>
+        <h1 className="text-2xl md:3xl font-bold   tracking-tight  text-neutral-600"> Karn Ashutosh Singh </h1>
+        <div>
+        </div>
+          <p className="text-neutral-600">&.Like to code and can tell me and build software  yara yara  and also passionate abou open source and
+          <p className="text-neutral-600"> &.yokoso and alos good at one thing com,muncatiing
+
+          </p>
+        </p>
+
+      </Container>
     </div>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  Container  from "@/components/containers"
+
+const Navbar = () => {
+  return (<Container>
+    <div className="w-full-h-full ">Navbar</div>
+    </Container>
+  )
+}
+
+export default Navbar
