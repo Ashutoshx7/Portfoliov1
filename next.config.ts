aa17dd833a1ee50aba9acf08bf108527cf56eeed
Karn Ashutosh  Singh <karns7844@gmail.com>
2025-11-11 03:31:07 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: false, // default true for Next 15 app router
+  },
 };
 
 export default nextConfig;
+
