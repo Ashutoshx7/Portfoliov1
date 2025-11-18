857e68729796abc9caae9c539b2c273bcabd2d6c
Karn Ashutosh  Singh <karns7844@gmail.com>
2025-11-18 10:40:44 +0530

    experience

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -59,7 +59,7 @@ export const Timeline = () => {
   return (
     <div>
 
-                  <h1 className="text-3xl md:text-3xl font-bold font-custom tracking-tight text-primary pb-5">
+                  <h1 className="text-3xl md:text-3xl font-bold font-custom tracking-tight text-primary pb-3 mt-5">
             <span className="link--elara">Experience</span>
           </h1>
     <div className="pl-1">
