4870d52b687361a82de0861b522357e90ecfe657
Karn Ashutosh  Singh <karns7844@gmail.com>
2025-11-18 23:43:47 +0530

    github

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Container from "@/components/containers"
 
 const Footer = () => {
   return (
-    <Container className=" flex justify-center border-y border-neutral-200 text-neutral-500 hover:text-primary font-custom2 tracking-normal text-xs p-2 ">
+    <Container className=" flex justify-center border-y border-neutral-200 text-neutral-500 hover:text-primary font-custom2 tracking-normal text-xs p-2 cursor-pointer">
         <p> Built with love By Karn Singh</p>
         <div className="flex items-center justify-center gap-4"> </div>
     </Container>
